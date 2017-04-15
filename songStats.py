f6e91fe563a07f487da2bbc57822f57bb3de76c9
ggoddx <ggoddx@umich.edu>
2017-04-15 13:43:47 -0400

    update songStats.py
    now calculates number of lines in a song as well as finds the language of the song

--- songStats.py
+++ songStats.py
@@ -1,10 +1,13 @@
-from keras.preprocessing.text import text_to_word_sequence as t2ws
+from sklearn.feature_extraction.text import CountVectorizer
 
 import csv, getSysArgs
-import numpy as np
+import langdetect as ld
 
 
 def main():
+    ## To ensure consistent identification of language for lyrics
+    ld.DetectorFactory.seed = 0
+
     ## CSV file of lyrics
     [fName] = getSysArgs.usage(['songStats.py', '<lyric_data_file_path>'])[1:]
 
@@ -14,18 +17,46 @@ def main():
     ## Column headers
     colNames = dataCSV.next()
 
+    ## Index of lyrics column
+    lyricI = colNames.index('lyrics')
+
     ## To store song statistics (assumes lyrics are in right-most column)
-    stats = [colNames[:-1] + ['lyricCharCt', 'lyricWordCt', 'lyricVocabSize']]
+    stats = [colNames[:-1] + ['lyricCharCt', 'lyricWordCt', 'lyricVocabSize', 
+                              'lines', 'lang']]
+
+    ## To obtain counts of words where strings of non-alphanumeric characters
+    #  are not considered words
+    tokenize = CountVectorizer().build_analyzer()
 
     for row in dataCSV:
+        print 'Number of song statistics collected: ', row[0]
+
+        ## Song's language
+        lang = None
+
         ## Lyrics for a song
-        lyrics = row[colNames.index('lyrics')]
+        lyrics = row[lyricI]
 
-        ## Word sequence from song lyrics
-        lyricSeq = t2ws(lyrics)
+        ## Lines in song
+        lines = lyrics.count('\n')
 
-        stats.append(row[:-1] + [len(lyrics), len(lyricSeq),
-                                 len(set(lyricSeq))])
+        ## Words in song
+        words = tokenize(lyrics)
+
+        ## Number of words in song
+        wrdCt = len(words)
+
+        if wrdCt < 1:
+            lang = 'none'
+        else:
+            try:
+                lang = ld.detect(lyrics.decode('utf-8'))
+            except ld.lang_detect_exception.LangDetectException:
+                lang = 'none'
+                print lyrics
+
+        stats.append(row[:-1] + [len(lyrics), wrdCt, len(set(words)), lines,
+                                 lang])
 
     ## File to write song statistics
     statsCSV = csv.writer(open('songStats.csv', 'wb', buffering = 0))
